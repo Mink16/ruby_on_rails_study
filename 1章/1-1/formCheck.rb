efebde95b63b4fad4f3c6876447a02218d38379c
MInk <minomax16@yahoo.co.jp>
2021-02-19 23:33:59 +0900

    誤字修正

--- 1章/1-1/formCheck.rb
+++ 1章/1-1/formCheck.rb
@@ -2,9 +2,9 @@
 
 # フォームから入力された文字列
 message = 'お宅の商品の発想から到着までがとても速くて助かりました。ただ、ちょっと箱がつぶれていたわよ'
-max_lengh = 30
+max_length = 30
 
 puts "入力メッセージ：#{message}"
-if message.length > max_lengh
-	puts "メッセージは#{max_lengh}文字以下で入力してください。"
+if message.length > max_length
+	puts "メッセージは#{max_length}文字以下で入力してください。"
 end
